d041d283907db981ff17ab34bb185dba7b28e6c2
Matias Karplus <matias@karplus.com.ar>
2024-08-28 17:48:49 -0300

    Added visual aid, DFS pathing done

--- Assets/GPT Finder/GraphManager.cs
+++ Assets/GPT Finder/GraphManager.cs
@@ -8,6 +8,10 @@ public class GraphManager : MonoBehaviour
 
     private List<Node> allNodes = new List<Node>();
 
+    [SerializeField]Material pathStartMaterial;
+    [SerializeField]Material pathEndMaterial;
+    [SerializeField]Material pathMaterial;
+
     private Pathfinding pathfinding = new Pathfinding();
 
     // Method to set the nodes created by MapManager
@@ -16,30 +20,34 @@ public class GraphManager : MonoBehaviour
         allNodes = nodes;
     }
 
-    void Start()
-    {
-        if (startNode != null && destinationNode != null)
-        {
-            // Perform a Depth-First Search to find a path to the destinationNode
-            bool pathFound = pathfinding.DepthFirstSearch(startNode, destinationNode);
-
-            if (pathFound)
-            {
-                Debug.Log("Path found to the destination!");
-            }
-            else
-            {
-                Debug.Log("Path not found to the destination.");
-            }
-        }
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
             // Reset the graph when the 'R' key is pressed
             pathfinding.ResetNodes(allNodes);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+
+            
+            if (startNode != null && destinationNode != null)
+            {
+                // Perform a Depth-First Search to find a path to the destinationNode
+                startNode.GetComponent<Renderer>().material = pathStartMaterial;
+                destinationNode.GetComponent<Renderer>().material = pathEndMaterial;
+                bool pathFound = pathfinding.DepthFirstSearch(startNode, destinationNode);
+
+                if (pathFound)
+                {
+                    Debug.Log("Path found to the destination!");
+                }
+                else
+                {
+                    Debug.Log("Path not found to the destination.");
+                }
+            }
         }
     }
 
